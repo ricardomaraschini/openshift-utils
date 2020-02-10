0b9c394814ff928f58fe8c627633139bcfda7f93
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2020-02-10 09:33:17 +0100

    customizations for my installation

--- v4/launch-dev-cluster.sh
+++ v4/launch-dev-cluster.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 
-# Environment variables
-# CLUSTER_DIR - local directory used to store installer artificats
-# PULL_SECRET - pull secret used to obtain the release payload (i.e. from try.openshift.com)
-# PAYLOAD_HOST - hostname for the OpenShift 4 payload (ex: quay.io)
-# PAYLOAD_IMAGE - Name of the image
-
 if [ $# -ne 1 ]; then
-  echo "Need release version, e.g. 4.0.0-0.ci-2019-04-15-011801"
-  exit 1
+	echo "Need release version, e.g. 4.0.0-0.ci-2019-04-15-011801"
+	exit 1
 fi
 
 version=$1
-clusterDir=${CLUSTER_DIR:-"$HOME/Code/openshift/clusters/dev-4"}
-pullSecret=${PULL_SECRET:-"$HOME/Code/openshift/clusters/secrets/pull-secret"}
-host=${PAYLOAD_HOST:-"registry.svc.ci.openshift.org"}
-image=${PAYLOAD_IMAGE:-"ocp/release"}
-
+clusterDir=/home/rmarasch/workspace/openshift-install/openshift-utils/cluster-aws
+pullSecret=/home/rmarasch/workspace/openshift-install/openshift-utils/PULL_SECRET
+host=registry.svc.ci.openshift.org
+image=ocp/release
 
 rm -rf /tmp/openshift-release
 mkdir /tmp/openshift-release
@@ -28,11 +21,11 @@ oc adm release extract --tools --to /tmp/openshift-release -a $pullSecret "$host
 
 mkdir -p /tmp/openshift-release/oc
 tar -xzf "openshift-client-linux-$version.tar.gz" -C /tmp/openshift-release/oc
-cp /tmp/openshift-release/oc/oc $HOME/bin
+cp /tmp/openshift-release/oc/oc $HOME/go/bin/
 
 mkdir -p /tmp/openshift-release/openshift-install
 tar -xzf "openshift-install-linux-$version.tar.gz" -C /tmp/openshift-release/openshift-install
-cp /tmp/openshift-release/openshift-install/openshift-install $HOME/bin
+cp /tmp/openshift-release/openshift-install/openshift-install $HOME/go/bin/
 
 echo "Destroying previous cluster at $clusterDir"
 
